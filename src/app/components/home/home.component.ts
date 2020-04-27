12304a4093adaf74676cf70b15f7160a3abe78af
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-04-27 12:33:17 +0300

    Added home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../crud.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  articles: object[] = [];
 
-  constructor() { }
+  constructor(private crud: CrudService, private router: Router) { }
 
   ngOnInit() {
+    this.getArticles();
   }
 
+  getArticles() {
+    this.crud.getArticles().subscribe(
+      res => {
+        for (let i = 0; i <= 4; i++) {
+          res.forEach(doc => {
+            this.articles.push({
+              title: doc.data().title,
+              content: doc.data().content,
+              path: doc.data().path,
+              ID: doc.id
+            });
+          });
+        }
+      }
+    );
+  }
+
+  readMore(ID: string) {
+    this.crud.getArticles().subscribe(
+      res => {
+        res.forEach(doc => {
+          if (ID === doc.id) {
+            sessionStorage.setItem('articleID', doc.id);
+            this.router.navigate(['articles/article']);
+          }
+        });
+      }
+    );
+  }
 }
