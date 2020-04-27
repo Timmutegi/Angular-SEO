8c1d8ecb1a05cff9e9b8c5f0f5c5d35754c60b92
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-04-27 12:35:07 +0300

    Added components to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './components/home/home.component';
+import { BlogpostsComponent } from './components/blogposts/blogposts.component';
+import { BlogpostdetailsComponent } from './components/blogpostdetails/blogpostdetails.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FrontendNavbarComponent } from './components/frontend-navbar/frontend-navbar.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BlogpostsComponent,
+    BlogpostdetailsComponent,
+    FrontendNavbarComponent,
+    FooterComponent,
+    HeroSectionComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
