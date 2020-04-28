5605c349e4c4b8434fe39d76ee7379c26fea396b
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-04-28 15:46:19 +0300

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { BlogpostsComponent } from './components/blogposts/blogposts.component';
+import { BlogpostdetailsComponent } from './components/blogpostdetails/blogpostdetails.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'articles', component: BlogpostsComponent },
+  { path: 'articles/:title', component: BlogpostdetailsComponent }
 ];
 
 @NgModule({
